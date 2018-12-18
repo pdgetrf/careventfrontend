14316a85bfcea7cbd68d34df37452aabb8ed5a32
lan <adwerrd@hotmail.com>
2018-12-17 16:14:55 -0800

    modify events routes

--- carevent/src/router.js
+++ carevent/src/router.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Router, Route, Switch } from 'dva/router'
-import IndexPage from './routes/Index/Index'
+import EventPage from './routes/Events/Index'
 import LoginPage from './routes/Login/index'
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Route path="/events" exact component={EventPage} />
         <Route path="/login" exact component={LoginPage} />
       </Switch>
     </Router>
